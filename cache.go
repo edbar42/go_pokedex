932812d2e277e95d9b409d5a1a008d6dede69644
Eduardo Barroso <dev@edbar.xyz>
2024-08-10 22:50:26 -0300

    draft: map command cache

--- cache.go
+++ cache.go
@@ -1,9 +1,29 @@
 package main
 
+import (
+	"time"
+
+	"github.com/edbar42/go_pokedex/api"
+)
+
 // Model for application wide cache
 type Cache struct {
-	NextMap *string
-	PrevMap *string
+	NextMap      *string
+	PrevMap      *string
+	MapCmdsCache map[string]MapCache
+}
+
+// Model for caching requests from the map and mapb commands
+type MapCache struct {
+	createdAt  time.Time
+	cachedData api.Regions
+}
+
+// TODO: Figure out how time.Time and time.Duration relate to each other
+func newMapCache(timeout time.Duration, r api.Regions) MapCache {
+	return MapCache{
+		createdAt: timeout,
+	}
 }
 
 func newCache() Cache {
